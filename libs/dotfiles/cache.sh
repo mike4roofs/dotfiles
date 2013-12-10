bed7f06bf45b0efa57a9ce0ee04470d6183ae9ea
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-12-10 22:11:39 +0100

    enable cache creation

--- libs/dotfiles/cache.sh
+++ libs/dotfiles/cache.sh
@@ -3,14 +3,12 @@ function create_cache_folder() {
 
   [[ -z "$dot_cache" ]] && e_abort "\$dot_cache is not set, aborting" 1
 
-  # mkdir -p $dot_cache
+  mkdir -p $dot_cache
 
-  # if [[ ! -d $dot_cache ]]; then
-  #   e_error "Cannot create folder. Aborting."
-  #   exit 1
-  # else
-  #   e_ok
-  # fi
-
-  e_arrow "pending"
+  if [[ ! -d $dot_cache ]]; then
+    e_error "Cannot create folder. Aborting."
+    exit 1
+  else
+    e_ok
+  fi
 }
