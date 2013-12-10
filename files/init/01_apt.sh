dddacca68a02047718614752c79e61379e692364
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-12-10 22:16:11 +0100

    update apt only on new installation

--- files/init/01_apt.sh
+++ files/init/01_apt.sh
@@ -1,4 +1,6 @@
 # Update APT.
-e_header "Updating APT"
-sudo apt-get -qq update
-sudo apt-get -qq dist-upgrade
+if [[ $new_dotfiles_install -eq 1 ]]; then
+  e_header "Updating APT"
+  sudo apt-get -qq update
+  sudo apt-get -qq dist-upgrade
+fi
