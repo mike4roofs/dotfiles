445ac5746c06d55e27466fd196c6778be6d2efe4
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-12-11 00:38:12 +0100

    update gitconfig user.email

--- files/closure/10_gitconfig.sh
+++ files/closure/10_gitconfig.sh
@@ -2,4 +2,4 @@
 read -e -p "Gitconfig | Set your email: " gitconfig_set_email
 echo $gitconfig_set_email
 
-# $(git config --global user.email $gitconfig_set_email)
+$(git config --global user.email $gitconfig_set_email)
