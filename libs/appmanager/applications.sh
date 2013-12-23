1a647fc2e7bd2f4b982faf17b3c7a23436352884
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-12-23 21:30:45 +0100

    appmanager: fix e_abort calling

--- libs/appmanager/applications.sh
+++ libs/appmanager/applications.sh
@@ -60,12 +60,11 @@ function uninstall_application() {
   REPLY="N"
   read -n 1 -p "Please confirm [y/N]: "
   echo ""
-  
+
   if [[ "$REPLY" =~ ^[Yy]$ ]]; then
     _do_action 'uninstall' $1
   else
-    e_abort "Uninstall not confirmed"
-    exit 1
+    e_abort "Uninstall not confirmed" 1
   fi
 }
 
