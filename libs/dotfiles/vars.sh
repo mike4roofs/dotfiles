562a8c1ef09c09711b0a6f8027d78e4676216702
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-12-23 22:36:36 +0100

    fix vars export

--- libs/dotfiles/vars.sh
+++ libs/dotfiles/vars.sh
@@ -3,16 +3,16 @@
 
 #  Calc path to dotfiles folder based on the relative position of the dotfiles
 #+ executable
-root_path=$(dirname $(dirname $0))
-# root_path="$HOME/.dotfiles"
+# export dot_root_path=$(dirname $(dirname $0))
+dot_root_path="$HOME/.dotfiles"
 
 # dotfiles main folder
-dot_folder=$root_path
+export dot_folder=$dot_root_path
 # dotfiles action files folder
-dot_files="$dot_folder/files"
+export dot_files="$dot_folder/files"
 # dotfiles submodules folder
-dot_libs="$dot_folder/libs"
+export dot_libs="$dot_folder/libs"
 # dotfiles library folder
-dot_lib="$dot_folder/libs/dotfiles"
+export dot_lib="$dot_folder/libs/dotfiles"
 # dotfiles cache folder
-dot_cache="$dot_folder/caches"
+export dot_cache="$dot_folder/caches"
