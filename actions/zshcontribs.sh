6c17d56825203a88ed6e59577f68fb484f51a9e6
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-12-23 23:00:12 +0100

    add zshcontrib actions

---
+++ actions/zshcontribs.sh
@@ -0,0 +1,22 @@
+# Initialize.
+function zshcontribs_header() { e_header "Linking Oh My Zsh contributions "; }
+
+function zshcontribs_do() {
+  local contrib=$dot_files/zshcontribs/$1
+  local link_folder=$dot_folder/libs/oh-my-zsh/contrib/
+
+  e_success "Linking contribution $1"
+
+  if [[ ! -d $link_folder ]]; then
+    mkdir $link_folder
+  fi
+
+  if [[ -d $link_folder ]]; then
+    ln -sf $contrib $link_folder
+  else
+    e_error "Can't create $link_folder"
+  fi
+
+}
+
+function zshcontribs_footer() { e_success "Completed"; }
