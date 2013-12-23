3e99f6d009f4243e6ef68b97b573d2066457a9f4
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-12-24 00:14:14 +0100

    run gitconfig closure only on first install

--- files/closure/10_gitconfig.sh
+++ files/closure/10_gitconfig.sh
@@ -1,5 +1,7 @@
 
-read -e -p "Gitconfig | Set your email: " gitconfig_set_email
-echo $gitconfig_set_email
+if [[ "$new_dotfiles_install" ]]; then
+  read -e -p "Gitconfig | Set your email: " gitconfig_set_email
+  echo $gitconfig_set_email
 
-$(git config --global user.email $gitconfig_set_email)
+  $(git config --global user.email $gitconfig_set_email)
+fi
