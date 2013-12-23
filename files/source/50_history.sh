6b69a398948ba1bf96fe7fbdbb23d217d7b3ba44
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-12-24 00:20:05 +0100

    fix shopt, which does not exists in zsh

--- files/source/50_history.sh
+++ files/source/50_history.sh
@@ -1,9 +1,9 @@
 # History settings
 
-# Allow use to re-edit a faild history substitution.
-shopt -s histreedit
+# Append instead of overwriting
+setopt appendhistory
 # History expansions will be verified before execution.
-shopt -s histverify
+setopt histverify
 
 # Entries beginning with space aren't added into history, and duplicate
 # entries will be erased (leaving the most recent entry).
