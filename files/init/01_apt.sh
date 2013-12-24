630f76a9db7627eadcce53fada6d72e9b0e68c9d
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-12-24 01:30:07 +0100

    init apt: install tree and cowsay

--- files/init/01_apt.sh
+++ files/init/01_apt.sh
@@ -4,3 +4,8 @@ if [[ $new_dotfiles_install -eq 1 ]]; then
   sudo apt-get -qq update
   sudo apt-get -qq dist-upgrade
 fi
+
+if [[ $new_dotfiles_install -eq 1 ]]; then
+  e_header "Installing tree, cowsay"
+  sudo apt-get -qq install tree cowsay
+fi
