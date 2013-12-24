26513b67aa1da9cb872cc5a7b65b3af3ad8c14ff
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-12-24 02:28:13 +0100

    move nave to init script

---
+++ files/init/50_nave.sh
@@ -0,0 +1,15 @@
+
+[[ ! "$(type node)" ]] && nave install stable
+[[ ! "$(type npm)" ]] && curl -sSL https://npmjs.org/install.sh | sh
+
+if [[ "$(type node)" ]]; then
+  nave use stable
+else
+  e_error "Can't install nodejs using nave"
+fi
+
+if [[ "$(type npm)" ]]; then
+  npm install -g yo bower grunt-cli linken node-inspector
+else
+  e_error "NPM is not installed for unknown reason"
+fi
--- files/source/50_nave.sh
+++
@@ -1,6 +0,0 @@
-
-nave use 0.10.24
-
-if [[ ! "$(type npm)" ]]; then
-  curl -sSL https://npmjs.org/install.sh | sh
-fi
