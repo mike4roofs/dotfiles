dcba26e323a2401f799fe151d00f6ecdde25ea7d
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-12-24 02:29:14 +0100

    update rvm on init + new rvm loading from source

--- files/init/50_rvm.sh
+++ files/init/50_rvm.sh
@@ -1,4 +1,15 @@
 
 if [[ ! "$(type rvm)" ]]; then
   \curl -sSL https://get.rvm.io | bash
+else
+  source "$HOME/.rvm/scripts/rvm"
+  rvm get head
+fi
+
+
+if [[ "$(type rvm)" ]]; then
+  rvm install 2.0.0
+  rvm --default use 2.0.0
+else
+  e_error "RVM can't be installed or updated for unknown reason"
 fi
--- files/source/50_rvm.sh
+++ files/source/50_rvm.sh
@@ -2,11 +2,8 @@
 # RVM
 #------------------------------
 
-# Added by RVM http://rvm.beginrescueend.com/
-[[ -s "/home/endorama/.rvm/scripts/rvm" ]] && source "/home/endorama/.rvm/scripts/rvm"  # This loads RVM into a shell session.
-
-# Load ruby 2 by default
-rvm use 2.0.0
+PATH=$PATH:$HOME/.rvm/bin
+export PATH
 
 # Autoloading .rvmrc files when found
 [[ -s ".rvmrc" ]] && source ".rvmrc"  # This loads RVM into a shell session.
