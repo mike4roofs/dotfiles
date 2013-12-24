637968a5c01731c918469cd01669300f6e034b62
Edoardo Tenani <edoardo.tenani@gmail.com>
2013-12-24 02:40:41 +0100

    fix npm installation of global commands

--- files/init/50_nave.sh
+++ files/init/50_nave.sh
@@ -1,21 +1,21 @@
 
-nave use stable
+nave use stable &> /dev/null
 
 [[ ! "$(type node)" ]] && nave install stable
 [[ ! "$(type npm)" ]] && curl -sSL https://npmjs.org/install.sh | sh
 
 if [[ "$(type node)" ]]; then
-  nave use stable
+  nave use stable &> /dev/null
 else
   e_error "Can't install nodejs using nave"
 fi
 
 if [[ "$(type npm)" ]]; then
-  npm install -g yo
-  npm install -g bower
-  npm install -g grunt-cli
-  npm install -g linken
-  npm install -g node-inspector
+  npm install -g yo &> /dev/null
+  npm install -g bower &> /dev/null
+  npm install -g grunt-cli &> /dev/null
+  npm install -g linken &> /dev/null
+  npm install -g node-inspector &> /dev/null
 else
   e_error "NPM is not installed for unknown reason"
 fi
