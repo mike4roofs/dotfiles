9aa234aaef73a8955b34d5234b2df283e7c77f04
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-01-04 02:46:43 +0100

    init: rvm and nave check only on first run

--- files/init/50_nave.sh
+++ files/init/50_nave.sh
@@ -1,40 +1,43 @@
-PATH=$PATH:$HOME/.dotfiles/bin
-source $HOME/.dotfiles/libs/shared/logging.sh
-source $HOME/.dotfiles/libs/dotfiles/test_for.sh
+if [[ $new_dotfiles_install -eq 1 ]]; then
 
-nave use stable 1>/dev/null
+  PATH=$PATH:$HOME/.dotfiles/bin
+  source $HOME/.dotfiles/libs/shared/logging.sh
+  source $HOME/.dotfiles/libs/dotfiles/test_for.sh
 
-function node_installer() {
-  nave install stable
-}
-test_for_command "node"
+  nave use stable 1>/dev/null
 
-function npm_installer() {
-  curl -sSL https://npmjs.org/install.sh | sh
-}
-test_for_command "npm"
+  function node_installer() {
+    nave install stable
+  }
+  test_for_command "node"
 
-function yo_installer() {
-  npm install -g yo 1> /dev/null
-}
-test_for_command "yo"
+  function npm_installer() {
+    curl -sSL https://npmjs.org/install.sh | sh
+  }
+  test_for_command "npm"
 
-function bower_installer() {
-  npm install -g bower 1> /dev/null
-}
-test_for_command "bower"
+  function yo_installer() {
+    npm install -g yo 1> /dev/null
+  }
+  test_for_command "yo"
 
-function grunt_installer() {
-  npm install -g grunt 1> /dev/null
-}
-test_for_command "grunt"
+  function bower_installer() {
+    npm install -g bower 1> /dev/null
+  }
+  test_for_command "bower"
 
-function linken_installer() {
-  npm install -g linken 1> /dev/null
-}
-test_for_command "linken"
+  function grunt_installer() {
+    npm install -g grunt 1> /dev/null
+  }
+  test_for_command "grunt"
 
-function node_inspector_installer() {
-  npm install -g node-inspector 1> /dev/null
-}
-test_for_command "node-inspector"
+  function linken_installer() {
+    npm install -g linken 1> /dev/null
+  }
+  test_for_command "linken"
+
+  function node_inspector_installer() {
+    npm install -g node-inspector 1> /dev/null
+  }
+  test_for_command "node-inspector"
+fi
