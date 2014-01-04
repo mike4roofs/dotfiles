0c7a8966af5a1c4b181ead3a7b0dfc0d9d9c9cc2
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-01-04 03:28:10 +0100

    little cleaning

--- files/source/11_prompt.sh
+++ files/source/11_prompt.sh
@@ -1,5 +1,7 @@
 autoload -U promptinit && promptinit
 autoload -U colors && colors
+
 # PROMPT='%{$fg_bold[green]%}[%n@%M%{$reset_color%} %{$fg_bold[blue]%}%~]$ %{$reset_color%}% $(git_super_status)'
 PROMPT='%{$fg_bold[green]%}[%M%{$reset_color%} %{$fg_bold[blue]%}%~]$ %{$reset_color%}% $(git_super_status)'
+
 RPROMPT='[%* on %D]' # prompt for right side of screen
