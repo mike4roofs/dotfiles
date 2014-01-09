a34d643d073c016f79a4406809a929101c5f5c6f
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-01-09 15:36:58 +0100

    source: fix composer PATH inclusion

--- files/source/50_composer.sh
+++ files/source/50_composer.sh
@@ -1,1 +1,13 @@
-export PATH=$HOME/.composer/vendor/bin:$PATH
+#
+# Composer specific source file
+
+# check if composer is installed
+hash composer 2>/dev/null
+if [[ $? -eq 0 ]]; then
+  # check if is already in the path
+  $(echo "$PATH" | grep -wq "$HOME/.composer/vendor/bin")
+  if [[ $? -eq 1 ]]; then
+    export PATH=$PATH:$HOME/.composer/vendor/bin
+  else
+  fi
+fi
