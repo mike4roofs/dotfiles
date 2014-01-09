f2a5478a6ac959e34a5cd80193c9ad00fe3ff8a2
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-01-09 15:50:22 +0100

    use .dotfiles/locals to store appmanager apps instead of /opt

--- libs/appmanager/vars.sh
+++ libs/appmanager/vars.sh
@@ -1,8 +1,8 @@
 # all available applications, will be filled afterwards
 export appmng_applications=()
 # application bin folder. Is included in the PATH from .bashrc and .zshrc
-export appmng_bin_dir="/opt/bin"
+export appmng_bin_dir="$HOME/.dotfiles/locals/bin"
 # destination folder in which application will be installed
-export appmng_dest_dir="/opt"
+export appmng_dest_dir="$HOME/.dotfiles/locals"
 # store command line arguments for passing them to run() function
 export appmng_cli_args=($@)
