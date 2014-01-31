41118ab0ce871ce588e755789dd5a2452f5d86e7
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-01-31 16:49:24 +0100

    load bashcompletion in zsh

--- files/source/05_zsh_options.sh
+++ files/source/05_zsh_options.sh
@@ -1,3 +1,7 @@
 setopt globdots
 unsetopt correct_all
 setopt correct
+
+# load bash completion | https://stackoverflow.com/questions/3249432/i-have-a-bash-tab-completion-script-is-there-a-simple-way-to-use-it-from-zsh
+autoload bashcompinit
+bashcompinit
