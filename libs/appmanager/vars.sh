5957d4bbe16cc99e8cfb38137868c2cad4de363b
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-02-11 18:48:58 +0100

    appmanager: fix application folder

--- libs/appmanager/vars.sh
+++ libs/appmanager/vars.sh
@@ -1,8 +1,8 @@
 # all available applications, will be filled afterwards
 export appmng_applications=()
 # application bin folder. Is included in the PATH from .bashrc and .zshrc
-export appmng_bin_dir="$HOME/.dotfiles/locals/bin"
+export appmng_bin_dir="/opt/bin"
 # destination folder in which application will be installed
-export appmng_dest_dir="$HOME/.dotfiles/locals"
+export appmng_dest_dir="/opt"
 # store command line arguments for passing them to run() function
 export appmng_cli_args=($@)
